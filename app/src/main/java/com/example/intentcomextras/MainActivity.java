e76c7f157b75bf6bdad68b9c1738eabb88b5641c
judsonpaiva <judson.paiva@hotmail.com>
2022-03-20 20:44:36 +0100

    configurando intents entre as activitys

--- app/src/main/java/com/example/intentcomextras/MainActivity.java
+++ app/src/main/java/com/example/intentcomextras/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.intentcomextras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edt_nome;
+    Button btn_enviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edt_nome = findViewById(R.id.edt_nome);
+        btn_enviar = findViewById(R.id.btn_enviar);
+
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nome = edt_nome.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("keyNome", nome);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/intentcomextras/MainActivity2.java
+++ app/src/main/java/com/example/intentcomextras/MainActivity2.java
@@ -2,13 +2,22 @@ package com.example.intentcomextras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView txt_resultado;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        txt_resultado = findViewById(R.id.txt_resultado);
+
+        intent = getIntent();
     }
 }
